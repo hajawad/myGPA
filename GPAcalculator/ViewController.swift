60f40a7ce4d8abfb533a5fd89fdc486dc579b4e9
AlJawad, Hashim <aljawad18@en208-m18122.local>
2017-03-03 15:27:01 -0800

    still not working....

--- GPAcalculator/ViewController.swift
+++ GPAcalculator/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate{
+    @IBOutlet weak var grade_field1: UITextField!
+    @IBOutlet weak var pickerView1: UIPickerView!
+    
+    var grades = ["A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView1 = UIPickerView();
+        pickerView1.delegate = self;
+        
+        self.grade_field1.inputView = self.pickerView1;
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int  {
+        return 1
     }
+    
+    private func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return grades.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        
+        return grades[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)  {
+        
+        grade_field1.text = grades[row]
 
+    }
 
 }
 
